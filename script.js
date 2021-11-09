935dcb03481046df94f60f68d46c9bcd7ddf78be
Anushka Chauhan <anushkachauhan1503@gmail.com>
2021-11-09 20:50:59 +0530

    JS: event listener for form submission

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 const charAmtRange = document.getElementById('charAmtRange')
 const charAmtNumber = document.getElementById('charAmtNumber')
 
+const includeUpperCaseElement = document.getElementById('includeUpperCase')
+const includeNumbersElement = document.getElementById('includeNumbers')
+const includeSymbolsElement = document.getElementById('includeSymbols')
+const form = document.getElementById('passwordGeneratorForm')
+
+/* Event listener for the form submission */
+form.addEventListener('submit', e => {
+    e.preventDefault()
+
+    const charAmt = charAmtNumber.value
+    const includeUpperCase = includeUpperCaseElement.checked
+    const includeNumbers = includeNumbersElement.checked
+    const includeSymbols = includeSymbolsElement.checked
+
+    const password = generatePassword(charAmt, includeUpperCase, includeNumbers, includeSymbols)
+    passwordDisplayElement.value = password
+});
+
 /* Syncing range and number inputs together */
 charAmtRange.addEventListener('input', syncCharAmt)
 charAmtNumber.addEventListener('input', syncCharAmt)
