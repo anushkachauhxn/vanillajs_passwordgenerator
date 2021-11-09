52dadafabb50be7f0d6ba53ac3ebe2747e3bf170
Anushka Chauhan <anushkachauhan1503@gmail.com>
2021-11-09 20:54:44 +0530

    JS: creating arrays of character codes

--- script.js
+++ script.js
@@ -19,6 +19,26 @@ form.addEventListener('submit', e => {
     passwordDisplayElement.value = password
 });
 
+/* Arrays of Character Codes */
+const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
+const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
+const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
+const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(
+    arrayFromLowToHigh(58, 64)
+).concat(
+    arrayFromLowToHigh(91, 96)
+).concat(
+    arrayFromLowToHigh(123, 126)
+)
+
+function arrayFromLowToHigh(low, high) {
+    const array = []
+    for (let i = low; i <= high; i++) {
+        array.push(i)
+    }
+    return array
+}
+
 /* Syncing range and number inputs together */
 charAmtRange.addEventListener('input', syncCharAmt)
 charAmtNumber.addEventListener('input', syncCharAmt)
